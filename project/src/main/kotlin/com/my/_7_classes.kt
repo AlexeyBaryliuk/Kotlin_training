1165079d88223eb63088e7e6834d152ed40f0d45
Aliaksei_Baryliuk <lex_a1@tut.by>
2021-12-30 11:57:02 +0300

    _7 classes getters

--- project/src/main/kotlin/com/my/_7_classes.kt
+++ project/src/main/kotlin/com/my/_7_classes.kt
@@ -1,2 +1,49 @@
 package com.my
 
+class PlainShort{}
+
+class ShortClass
+
+class WithConstructor constructor(name: String, age: Int)
+
+class WithoutConstructor(name: String, age: Int)
+
+class MandatoryConstructor internal constructor(name: String, age: Int){}
+
+class CurseProperty(courseTitle: String){
+    val title = courseTitle
+}
+
+class PersonClass(name: String, age: Int){}
+
+
+fun main(){
+    val course = Course("course")
+    course.description = "description"
+    course.show()
+    println(course)
+
+    course.description = ""
+    course.someStr = ""
+
+    println(course)
+}
+class Course(courseTitle: String){
+    private val title = courseTitle
+    internal var description = ""
+        get(){
+            return if (field.isNullOrBlank()) "no description " else "$field"
+        }
+    var someStr = "someString"
+    get(){
+        return if (field.isNullOrBlank()) "no description " else "$field"
+    }
+
+    fun show(){
+        println("Title is $title")
+    }
+
+    override fun toString(): String {
+        return "($title) and ($description) and ($someStr)"
+    }
+}
